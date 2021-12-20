da78a4576eadcff245a13edf5b509619722c70dc
Alan Stelmach <alan.stelmach1@gmail.com>
2021-12-20 22:24:18 +0100

    Init commit

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 0.1f);
+    }
+}
