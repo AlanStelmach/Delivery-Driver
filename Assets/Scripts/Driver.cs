c76903dfb75ae817ab4edeea74f38170d2c45638
Alan Stelmach <alan.stelmach1@gmail.com>
2021-12-21 21:05:22 +0100

    Added controles

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -14,7 +14,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
